14e62463681a65d7a61926bf5d3d278617892e0d
skuuu <saanakuu.u@gmail.com>
2018-10-16 12:35:51 +0800

    tietokanta heroku

--- src/main/java/tikape/harjoitustyokaksi/Database.java
+++ src/main/java/tikape/harjoitustyokaksi/Database.java
@@ -10,7 +10,17 @@ public class Database {
         this.databaseAddress = databaseAddress;
     }
 
+//    public Connection getConnection() throws SQLException {
+//        return DriverManager.getConnection(databaseAddress);
+//    }
+    
+    
     public Connection getConnection() throws SQLException {
+    String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return DriverManager.getConnection(dbUrl);
+        }
+
         return DriverManager.getConnection(databaseAddress);
     }
 
